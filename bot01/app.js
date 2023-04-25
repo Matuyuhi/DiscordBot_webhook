c4f030c86e3c35e3fc043ddfea5d6deb240cff35
Matuyuhi <bird9.yuhi@gmail.com>
2023-04-25 18:17:45 +0900

    wip

--- bot01/app.js
+++ bot01/app.js
@@ -363,8 +363,14 @@ String.prototype.replaceUsersLink = function (replaces) {
         const base = String(data.gitname)
         const place = '<@!' + String(data.id) + '>'
         if (out.match(base)) {
-            out = out.replace(new RegExp('(^|[^\\w\\/])@' + base, 'g'), place)
-            out = out.replace(new RegExp('(^|[^\\w\\/])' + base, 'g'), place)
+            out = out.replace(
+                new RegExp('(^|[^\\w\\/])@' + base + '(^|[^\\w\\/])', 'g'),
+                place
+            )
+            out = out.replace(
+                new RegExp('(^|[^\\w\\/])' + base + '(^|[^\\w\\/])', 'g'),
+                place
+            )
         }
     }
     return out
